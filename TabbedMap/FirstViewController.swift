733f3517082b2b0a6aba76a54c9663f8a21f14b9
Ulrik Nilsson <ulrik.nilsson@evry.com>
2017-12-11 21:13:40 +0100

    Initial Commit

---
+++ TabbedMap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedMap
+//
+//  Created by Ulrik Nilsson on 2017-12-11.
+//  Copyright © 2017 Ulrik Nilsson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TabbedMap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabbedMap
+//
+//  Created by Ulrik Nilsson on 2017-12-11.
+//  Copyright © 2017 Ulrik Nilsson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
